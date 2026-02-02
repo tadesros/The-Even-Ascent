9935067e4a43ac849df89d15bb5e51071c15f58a
Tom <tomadesrosiers@gmail.com>
2026-02-01 22:58:31 -0500

    Guthub

--- js/site.js
+++ js/site.js
@@ -1,8 +1,33 @@
 
-//Call Hello World
-function HelloWorld() {
+//Get the Values From the Page
+//Starts or controller function
+function getValues(){
 
-  alert("Hello, World!");
+  //Get Values from the Page
+    let startValue = document.getElementById("startValue").value;
+
+  //We call Generate Numbers
+``
+
+  //We call Display Numbers
+
+
+}
+
+//Generate numbers from the start value to the end value
+//Logic Function (s)
+function generateNumbers(){
+
+   
+
+
+}
+
+//Display the numbers and highlight the even numbers in bold
+//Display or view function
+function displayNumbers(){
+
+
 
 }
 
